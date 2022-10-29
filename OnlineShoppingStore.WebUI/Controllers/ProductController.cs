708ff6f71d4efffa3833bff12da1e1cd405e890f
Padmini <Padmini@LAPTOP-5V5JDH1L>
2022-10-29 20:02:28 +0530

    Add project files.

---
+++ OnlineShoppingStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using ShoppingStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository productRepository;
+        public ProductController(IProductRepository repo)
+        {
+            productRepository = repo;
+        }
+       public ViewResult List()
+        {
+            return View(productRepository.Products);
+        }
+    }
+}
